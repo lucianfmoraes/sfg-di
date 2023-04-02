533ad11ad3ed02842902c3f80fa453e6a239692f
Lucian Moraes <lucianfmoraes@gmail.com>
2023-04-02 14:47:14 -0300

    section 3 class 37

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -15,16 +15,22 @@ public class SfgDiApplication {
 
 		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
 		MyController myController = (MyController) context.getBean("myController");
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+
+		System.out.println(" ");
+
+		System.out.println(" * * * * * Primary Bean * * * * *");
+		System.out.println(myController.sayHello());
+		System.out.println(" ");
 
 		System.out.println(" * * * * * Property Injection * * * * *");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) context.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
+		System.out.println(" ");
 
 		System.out.println(" * * * * * Setter Injection * * * * *");
 		SetterInjectedController setterInjectedController = (SetterInjectedController) context.getBean("setterInjectedController");
 		System.out.println(setterInjectedController.getGreeting());;
+		System.out.println(" ");
 
 		System.out.println(" * * * * * Constructor Injection * * * * *");
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) context.getBean("constructorInjectedController");
